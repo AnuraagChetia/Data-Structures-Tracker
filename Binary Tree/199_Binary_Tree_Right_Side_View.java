afbfeee068bd48304e2211224970647df8e6a821
Anuraag Chetia <anuraagchetia77@gmail.com>
2026-08-06 21:41:02 +0530

    leetcode 199 right side view of BT

---
+++ Binary Tree/199_Binary_Tree_Right_Side_View.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.TreeMap;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+// INTUITION -
+// BFS
+// For each level, store the right-most node
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode curr = q.poll();
+                if (i == levelSize - 1)
+                    result.add(curr.val);
+                if (curr.left != null)
+                    q.offer(curr.left);
+                if (curr.right != null)
+                    q.offer(curr.right);
+            }
+        }
+        return result;
+    }
+}
